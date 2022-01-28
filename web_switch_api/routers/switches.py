651f2c4b0db1b8bfe148e140961d2649535d8751
Tomas Zikmund <tommekster@gmail.com>
2022-01-28 01:19:45 +0100

    switches - update_switch implementation

--- web_switch_api/routers/switches.py
+++ web_switch_api/routers/switches.py
@@ -1,6 +1,14 @@
 from fastapi import APIRouter, Depends, HTTPException
-
+from typing import List
+from pydantic import BaseModel
 from ..dependencies import get_token_header
+
+
+class Switch(BaseModel):
+    id: int
+    label: str
+    switched: bool
+
 
 router = APIRouter(
     prefix="/switches",
@@ -10,16 +18,20 @@ router = APIRouter(
 )
 
 fake_switches_db = [
-    {"id": 0, "label": "Swicth 1", "switched": True},
-    {"id": 1, "label": "Swicth 2", "switched": False},
+    Switch(id=0, label="Swicth 1", switched=True),
+    Switch(id=1, label="Swicth 2", switched=False),
 ]
 
 
-@router.get("/")
+@router.get("/", response_model=List[Switch])
 async def get_switches():
     return fake_switches_db
 
 
-@router.put("/{switch_id}")
-async def update_switch(switch_id: str):
-    pass
+@router.put("/{switch_id}", response_model=Switch)
+async def update_switch(switch_id: int, update: Switch):
+    switch = next((x for x in fake_switches_db if x.id == switch_id), None)
+    if not switch:
+        raise HTTPException(status_code=404, detail="Switch is missing")
+    switch.switched = update.switched
+    return switch
