be9120dba41de85b74c29543f7d16a71e19d9892
Tomas Zikmund <tommekster@gmail.com>
2022-01-28 10:34:46 +0100

    enable CORS

--- web_switch_api/main.py
+++ web_switch_api/main.py
@@ -1,9 +1,18 @@
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .dependencies import get_token_header
 from .routers import switches, captiveImage, users
 
 app = FastAPI(dependencies=[Depends(get_token_header)])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(switches.router)
 app.include_router(captiveImage.router)
