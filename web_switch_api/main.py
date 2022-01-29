335b1bcee95ff461c8c6a6b86268848a7aa4b694
Tomas Zikmund <tommekster@gmail.com>
2022-01-29 17:17:11 +0100

    host react app

--- web_switch_api/main.py
+++ web_switch_api/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 from .routers import switches, captiveImage, users, authentication
+from .react import ReactStaticFiles
 
 app = FastAPI()
 
@@ -12,12 +13,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(switches.router)
-app.include_router(captiveImage.router)
-app.include_router(users.router)
-app.include_router(authentication.router)
+router = APIRouter(prefix="/api")
+router.include_router(switches.router)
+router.include_router(captiveImage.router)
+router.include_router(users.router)
+router.include_router(authentication.router)
+
+app.include_router(router)
+
+react = ReactStaticFiles(directory='../web_switch_gui/build', html=True)
+app.mount('/', react, name='whatever')
 
 
-@app.get("/")
+@app.get("/hello")
 async def root():
     return {"message": "Hello world"}
