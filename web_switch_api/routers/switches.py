b2e9beb9f1161f2b8925a253bf5a663df840f80b
Tomas Zikmund <tommekster@gmail.com>
2022-01-30 01:14:07 +0100

    switches - call background api

--- web_switch_api/routers/switches.py
+++ web_switch_api/routers/switches.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from typing import List
+from typing import List, Optional
 from pydantic import BaseModel
+import requests
 from ..dependencies import RequireRole
+from .. import configuration
 
 
 class Switch(BaseModel):
@@ -9,6 +11,8 @@ class Switch(BaseModel):
     label: str
     switched: bool
 
+
+config = configuration.provider.get_switches_config()
 
 require_role = RequireRole("ROLE_SWITCH")
 
@@ -19,24 +23,37 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-fake_switches_db = [
-    Switch(id=0, label="Swicth 1", switched=True),
-    Switch(id=1, label="Swicth 2", switched=False),
-]
+
+def __call_switch_api__(switch_id: Optional[int] = None, new_state: Optional[bool] = None) -> List[Switch]:
+    action = "on" if new_state else "off"
+    url = (
+        config.api_url +
+        (f"/{switch_id}/{action}" if switch_id is not None else "")
+    )
+    response = requests.get(url)
+    switches = [
+        Switch(
+            id=int(k),
+            switched=v,
+            label=config.switch_names.get(int(k), f"switch {k}")
+        )
+        for k, v in response.json().items()
+    ]
+    return switches
 
 
 @router.get("/", response_model=List[Switch])
 async def get_switches():
-    return fake_switches_db
+    return __call_switch_api__()
 
 
 @router.put("/{switch_id}", response_model=Switch)
 async def update_switch(switch_id: int, update: Switch):
-    switch = next((x for x in fake_switches_db if x.id == switch_id), None)
+    switches = __call_switch_api__(switch_id, update.switched)
+    switch = next((x for x in switches if x.id == switch_id), None)
     if not switch:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Switch is missing"
         )
-    switch.switched = update.switched
     return switch
